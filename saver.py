2417030abcc4bc777aacbb7e771c8b326fff19b8
Alexander Makarov <alexm@yandex-team.ru>
2012-10-12 17:20:55 +0400

    scale, smooth, center

--- saver.py
+++ saver.py
@@ -4,13 +4,14 @@ import pygame
 import pygame.image
 import pygame.display
 import pygame.event
+import pygame.transform
 import time
 import sys
 import urllib2
 from xml.dom import minidom
 
 TMP = "/tmp/image.jpg"
-SLEEP_SECONDS = 1
+SLEEP_SECONDS = 2
 
 def update():
   src = urllib2.urlopen("http://fotki.yandex.ru/live/rss2")
@@ -29,16 +30,32 @@ def download(item):
 
 def display():
   picture = pygame.image.load(TMP)
-  surface.fill(0)
-  surface.blit(picture, (0, 0))
+  (width, height) = picture.get_size()
+  ratioH = float(screen_height) / height
+  ratioV = float(screen_width) / width
+  ratio = min(ratioH, ratioV)
+  newsize = (int(width * ratio), int(height* ratio))
+  if picture.get_bitsize() < 24:
+    picture = pygame.transform.scale(picture, newsize)
+  else:
+    picture = pygame.transform.smoothscale(picture, newsize)
+  dx = (screen_width - picture.get_width()) / 2
+  dy = (screen_height - picture.get_height()) / 2
+ 
+  buf = surface.copy() 
+  buf.fill(0)
+  buf.blit(picture, (dx, dy))
+  surface.blit(buf, (0, 0))
   pygame.display.update()
   time.sleep(SLEEP_SECONDS)
   if pygame.event.peek(pygame.KEYDOWN):
     sys.exit(0)
 
 
-surface = pygame.display.set_mode((0, 0))
-#, pygame.FULLSCREEN)
+surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+info = pygame.display.Info()
+screen_width = info.current_w
+screen_height = info.current_h
 
 while True:
   items = update()
